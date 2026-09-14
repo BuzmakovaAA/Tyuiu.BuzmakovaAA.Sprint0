7145590798733fc69f20d4be41bd0a2d620fee8f
BuzmakovaAA <ulinichbuzmak@mail.ru>
2026-09-15 04:15:59 +0500

    Task2

---
+++ Tyuiu.Buzmakova.Sprint0.Task2.V0.Lib/DataService.cs
@@ -0,0 +1,9 @@
+﻿namespace Tyuiu.Buzmakova.Sprint0.Task2.V0.Lib
+{
+    public class DataService
+    {
+        public static string GetMessage(String name);
+        {
+        return $"Привет, {name}";
+    }
+}
---
+++ Tyuiu.BuzmakovaAA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.BuzmakovaAA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.BuzmakovaAA.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Алина"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
